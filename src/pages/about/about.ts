797945c7df780ae1c183f2df225b542cd9df793c
Walaa Fadil <walaafathel@yahoo.com>
2018-08-15 00:43:49 +0300

    update

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,34 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  searchQuery: string = '';
+  items: string[];
 
+  constructor() {
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      'مركز صحي واسط',
+      'مركز صحي الجامعة',
+      'مركز صحي عرفة'
+    
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,7 +8,9 @@ import { NavController } from 'ionic-angular';
 export class ContactPage {
 
   constructor(public navCtrl: NavController) {
+    
 
   }
+  
 
 }
